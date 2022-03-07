ffb71f2d1de84888d145330c2082f3df081b77f2
Julien Bt <julienbt@protonmail.com>
2022-03-07 19:21:53 +0100

    Add `add` function

--- src/lib.rs
+++ src/lib.rs
@@ -1,8 +1,16 @@
+/// Integers addition
+pub fn add(a: i32, b: i32) -> i32
+{
+    a + b
+}
+
+
 #[cfg(test)]
 mod tests {
     #[test]
-    fn it_works() {
-        let result = 2 + 2;
-        assert_eq!(result, 4);
+    fn test_add()
+    {
+        use super::add;
+        assert_eq!(add(1, 1), 2);
     }
 }
